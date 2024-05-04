6ab2d4bd8855629abd62dc35b2e6610faa4d846f
MD ABIDUR RAHMAN NAFIM <M.R.NAFIM-2022@hull.ac.uk>
2024-05-04 15:29:45 +0100

    Add project files.

---
+++ Summative 6/Circle.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Summative_6
+{
+     class Circle
+     {
+        private static int lastID = 0;
+        private int ID;
+        private double radius;
+
+        public Circle(double radius)
+        {
+            SetRadius(radius);
+            ID = ++lastID;
+        }
+
+        public Circle() : this(1) { } // Default constructor with a radius of 1
+
+        public void SetRadius(double newRadius)
+        {
+            if (newRadius >= 0)
+            {
+                radius = newRadius;
+            }
+            else
+            {
+                Console.WriteLine("Radius cannot be negative. Radius set to 0.");
+                radius = 0;
+            }
+        }
+
+        public double GetCircumference()
+        {
+            return 2 * Math.PI * radius;
+        }
+
+        public double GetArea()
+        {
+            return Math.PI * radius * radius;
+        }
+
+        public int GetID()
+        {
+            return ID;
+        }
+     }
+}
---
+++ Summative 6/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Summative_6;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("try entering a radius for the circle:");
+        double radius = GetRadiusFromUser();
+
+        Circle instance1 = new Circle(radius);
+        Console.WriteLine($"Circle with ID {instance1.GetID()} has been created.");
+        Console.WriteLine($"Circumference would be {instance1.GetCircumference():F2} and area would be {instance1.GetArea():F2}");
+    }
+
+    static double GetRadiusFromUser()
+    {
+        while (true)
+        {
+            try
+            {
+                double input = double.Parse(Console.ReadLine());
+                if (input >= 0)
+                {
+                    return input;
+                }
+                else
+                {
+                    Console.WriteLine("try to enter a non-negative number.");
+                }
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid input. try to enter a valid number.");
+            }
+        }
+    }
+}
+
